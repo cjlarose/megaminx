b28a8b6c9f839d801efa784987b4ae07f01a3bb9
Chris LaRose <chris.larose@mycase.com>
2015-11-21 11:28:55 -0800

    cjl - Add regular pentagon

--- src/megaminx/core.cljs
+++ src/megaminx/core.cljs
@@ -9,10 +9,28 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn hello-world []
-  [:h1 (:text @app-state)])
+(def central-angle (/ (* Math.PI 2) 5))
 
-(reagent/render-component [hello-world]
+(defn regular-pentagon [r]
+  (let [theta (/ central-angle 2)
+        b (* r (Math/sin theta))
+        h (* r (Math/cos theta))
+        triangle (fn [i]
+                   [:div {:style {:border-style "solid"
+                                  :margin-left (str (- b) "em")
+                                  :margin-top (str (/ h -2) "em")
+                                  :transform (str "rotateZ(" (* i central-angle) "rad) translateY(" (/ h 2) "em)")
+                                  :border-color "transparent transparent #f00"
+                                  :border-width (str "0 " b "em " h "em")}}])]
+    [:div {}
+     (map triangle (range 5))]))
+
+(defn scene []
+  [:div
+    [:h1 (:text @app-state)]
+    (regular-pentagon 5)])
+
+(reagent/render-component [scene]
                           (. js/document (getElementById "app")))
 
 
