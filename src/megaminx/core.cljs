2e9c94e1d5bf84383b387fe50b8f3e3f1520e861
Chris LaRose <chris.larose@mycase.com>
2015-11-21 15:33:55 -0800

    cjl - Half of dodecahedron, guestimated values

--- src/megaminx/core.cljs
+++ src/megaminx/core.cljs
@@ -11,7 +11,7 @@
 
 (def central-angle (/ (* Math.PI 2) 5))
 
-(defn regular-pentagon [r]
+(defn regular-pentagon [r color transform]
   (let [theta (/ central-angle 2)
         b (* r (Math/sin theta))
         h (* r (Math/cos theta))
@@ -20,15 +20,27 @@
                                   :margin-left (str (- b) "em")
                                   :margin-top (str (/ h -2) "em")
                                   :transform (str "rotateZ(" (* i central-angle) "rad) translateY(" (/ h 2) "em)")
-                                  :border-color "transparent transparent #f00"
+                                  :border-color (str "transparent transparent " color)
                                   :border-width (str "0 " b "em " h "em")}}])]
-    [:div {}
+    [:div {:style {:transform transform}}
      (map triangle (range 5))]))
 
+(defn dodecahedron [r]
+  (let [r 5.7
+        r-x -26]
+    [:div
+      ;(regular-pentagon 5 "#000" "")
+      (regular-pentagon 5 "#0f0" (str "rotateX(90deg) translateZ(3.6em)"))
+      (regular-pentagon 5 "#f0f" (str "rotateY(0deg)   rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))
+      (regular-pentagon 5 "#00f" (str "rotateY(72deg)  rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))
+      (regular-pentagon 5 "#ff0" (str "rotateY(144deg) rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))
+      (regular-pentagon 5 "#f00" (str "rotateY(216deg) rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))
+      (regular-pentagon 5 "#fff" (str "rotateY(288deg) rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))]))
+
 (defn scene []
-  [:div
+  [:div {:style {:transform "rotateX(-30deg)"}}
     [:h1 (:text @app-state)]
-    (regular-pentagon 5)])
+    (dodecahedron 5)])
 
 (reagent/render-component [scene]
                           (. js/document (getElementById "app")))
