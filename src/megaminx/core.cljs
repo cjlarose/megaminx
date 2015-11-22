013a78a00408acdc7c0c5c0389894b1409b4a3c1
Chris LaRose <chris.larose@mycase.com>
2015-11-21 18:55:05 -0800

    cjl - Half-dodecahedro, calculated values

--- src/megaminx/core.cljs
+++ src/megaminx/core.cljs
@@ -9,33 +9,47 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
-(def central-angle (/ (* Math.PI 2) 5))
+(def phi (/ (+ 1 (js/Math.sqrt 5)) 2))
 
-(defn regular-pentagon [r color transform]
-  (let [theta (/ central-angle 2)
-        b (* r (Math/sin theta))
-        h (* r (Math/cos theta))
+(defn central-angle [n]
+  (/ (* Math.PI 2) n))
+
+(defn apothem [n s]
+  (let [theta (/ js/Math.PI n)]
+    (/ s (* 2 (js/Math.tan theta)))))
+
+(defn circumradius [n s]
+  (let [theta (/ js/Math.PI n)]
+    (/ s (* 2 (js/Math.sin theta)))))
+
+(defn regular-pentagon [s color transform]
+  (let [b (/ s 2)
+        h (apothem 5 s)
         triangle (fn [i]
                    [:div {:style {:border-style "solid"
                                   :margin-left (str (- b) "em")
                                   :margin-top (str (/ h -2) "em")
-                                  :transform (str "rotateZ(" (* i central-angle) "rad) translateY(" (/ h 2) "em)")
+                                  :transform (str "rotateZ(" (* i (central-angle 5)) "rad) translateY(" (/ h 2) "em)")
                                   :border-color (str "transparent transparent " color)
                                   :border-width (str "0 " b "em " h "em")}}])]
     [:div {:style {:transform transform}}
      (map triangle (range 5))]))
 
-(defn dodecahedron [r]
-  (let [r 5.7
-        r-x -26]
+(defn dodecahedron [s]
+  (let [a (apothem 5 s)
+        R (circumradius 5 s)
+        dihedral-angle (- js/Math.PI (js/Math.atan 2))
+        r-x (- (/ js/Math.PI 2) dihedral-angle)
+        r (/ (* s (js/Math.pow phi 3)) (* 2 (js/Math.sqrt (+ (js/Math.pow phi 2) 1))))]
     [:div
+      dihedral-angle "rad"
       ;(regular-pentagon 5 "#000" "")
-      (regular-pentagon 5 "#0f0" (str "rotateX(90deg) translateZ(3.6em)"))
-      (regular-pentagon 5 "#f0f" (str "rotateY(0deg)   rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))
-      (regular-pentagon 5 "#00f" (str "rotateY(72deg)  rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))
-      (regular-pentagon 5 "#ff0" (str "rotateY(144deg) rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))
-      (regular-pentagon 5 "#f00" (str "rotateY(216deg) rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))
-      (regular-pentagon 5 "#fff" (str "rotateY(288deg) rotateZ(180deg) translateZ(" r "em) rotateX(" r-x "deg)"))]))
+      (regular-pentagon s "#0f0" (str "rotateX(90deg) translateZ(" r "em)"))
+      (regular-pentagon s "#f0f" (str "rotateY(0deg)   rotateZ(180deg) rotateX(" r-x "rad) translateZ(" r "em)"))
+      (regular-pentagon s "#00f" (str "rotateY(72deg)  rotateZ(180deg) rotateX(" r-x "rad) translateZ(" r "em)"))
+      (regular-pentagon s "#ff0" (str "rotateY(144deg) rotateZ(180deg) rotateX(" r-x "rad) translateZ(" r "em)"))
+      (regular-pentagon s "#f00" (str "rotateY(216deg) rotateZ(180deg) rotateX(" r-x "rad) translateZ(" r "em)"))
+      (regular-pentagon s "#fff" (str "rotateY(288deg) rotateZ(180deg) rotateX(" r-x "rad) translateZ(" r "em)"))]))
 
 (defn scene []
   [:div {:style {:transform "rotateX(-30deg)"}}
