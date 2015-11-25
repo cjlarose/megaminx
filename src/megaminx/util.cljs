cf91421cef3f73910dd74aab8baea8ae3c27c486
Chris LaRose <chris.larose@mycase.com>
2015-11-24 17:44:23 -0800

    cjl - Add interior angle util

--- src/megaminx/util.cljs
+++ src/megaminx/util.cljs
@@ -5,6 +5,9 @@
 (defn central-angle [n]
   (/ (* Math.PI 2) n))
 
+(defn interior-angle [n]
+  (* (- n 2) js/Math.PI (/ n)))
+
 (defn apothem [n s]
   (let [theta (/ js/Math.PI n)]
     (/ s (* 2 (js/Math.tan theta)))))
