c9400d93fbb3c2092d29a7d47a0868491875c0be
Chris LaRose <chris.larose@mycase.com>
2015-12-01 20:04:23 -0800

    cjl - Fix translate-z for front and back faces
    All examples where one face is a non-square work!

--- src/megaminx/core.cljs
+++ src/megaminx/core.cljs
@@ -86,14 +86,14 @@
   (let [[skew-alpha skew-beta skew-gamma] (map #(- % (/ js/Math.PI 2)) [alpha beta gamma])]
     [:div
      [transform ;; front
-      [(m/translate-z (* s (js/Math.sin beta) 0.5))
+      [(m/translate-z (* s (js/Math.sin gamma) (js/Math.sin beta) 0.5))
        (m/translate-y (* s (js/Math.cos beta) 0.5))
        (m/translate-x (- (* s (js/Math.cos gamma) 0.5)))
        (m/skew-x skew-alpha)
        (m/scale-y (js/Math.cos skew-alpha))]
       (component/rect {:w s :h s} {:background-color "#9966ff" :opacity 0.5})]
      [transform ;; back
-      [(m/translate-z (- (* s (js/Math.sin beta) 0.5)))
+      [(m/translate-z (- (* s (js/Math.sin gamma) (js/Math.sin beta) 0.5)))
        (m/translate-y (- (* s (js/Math.cos beta) 0.5)))
        (m/translate-x (* s (js/Math.cos gamma) 0.5))
        (m/rotate-y js/Math.PI)
