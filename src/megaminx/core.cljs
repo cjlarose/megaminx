5271460a1e5c0b1184f9772c1bfbfe7fd559c77f
Chris LaRose <chris.larose@mycase.com>
2015-12-04 21:22:00 -0800

    cjl - Initialize gl context

--- src/megaminx/core.cljs
+++ src/megaminx/core.cljs
@@ -1,14 +1,5 @@
 (ns megaminx.core
-  (:require [reagent.core :as reagent :refer [atom]]
-            [megaminx.util :refer [central-angle
-                                   interior-angle
-                                   dihedral-angle
-                                   circumradius
-                                   phi
-                                   hypotenuse
-                                   apothem]]
-            [megaminx.component :refer [transform] :as component]
-            [megaminx.model :as m]))
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
@@ -19,172 +10,20 @@
 (defonce app-state (atom {:rotate-y "Hello world!"
                           :rotate-x ""}))
 
-(defn regular-pentagon [s color]
-  (let [h (apothem 5 s)]
-    [:div
-     (map (fn [i] (with-meta
-                    [transform
-                     [(m/rotate-z (* i (central-angle 5)))
-                      (m/translate-y (/ h 2))]
-                     (component/isosceles-triangle {:b s :h h :color color} {})]
-                    {:key i}))
-          (range 5))]))
-
-(defn pentagonal-pyramid [s h color]
-  (let [c (central-angle 5)
-        a (apothem 5 s)
-        lateral-height (hypotenuse a h)
-        base-angle (js/Math.atan (/ h a))]
-    [:div
-     [transform
-      [(m/rotate-x (- (/ js/Math.PI 2)))
-       (m/translate-z (/ h 2))]
-      [regular-pentagon s color]]
-     (map-indexed (fn [i] (with-meta
-                            [transform
-                             [(m/rotate-y (+ (* i c) (/ c 2)))
-                              (m/translate-z (/ a 2))
-                              (m/rotate-x (- (/ js/Math.PI 2) base-angle))]
-                             [component/isosceles-triangle {:b s :h lateral-height :color color} {}]]
-                            {:key i}))
-                  (range 5))]))
-
-(defn dodecahedron [s]
-  (let [a (apothem 5 s)
-        c (central-angle 5)
-        R (circumradius 5 s)
-        r-x (- (/ js/Math.PI 2) dihedral-angle)
-        r (/ (* s (js/Math.pow phi 3)) (* 2 (js/Math.sqrt (+ (js/Math.pow phi 2) 1))))]
-    [:div
-     [transform
-       [(m/rotate-x (/ js/Math.PI 2))
-        (m/translate-z r)]
-       [regular-pentagon s "#00ff00"]]
-     [transform
-       [(m/rotate-x (- (/ js/Math.PI 2)))
-        (m/translate-z r)]
-       [regular-pentagon s "#8bc34a"]]
-     (map-indexed (fn [i color] (with-meta
-                                  [transform
-                                   [(m/rotate-y (* c i))
-                                    (m/rotate-z js/Math.PI)
-                                    (m/rotate-x r-x)
-                                    (m/translate-z r)]
-                                   [regular-pentagon s color]]
-                                  {:key (+ i 2)}))
-                  ["#800080" "#0000ff" "#ffff00" "#ff0000" "#ffffff"])
-     (map-indexed (fn [i color] (with-meta
-                                  [transform
-                                   [(m/rotate-y (+ (* c i) (/ c 2)))
-                                    (m/rotate-x r-x)
-                                    (m/translate-z r)]
-                                   [regular-pentagon s color]]
-                                  {:key (+ i 7)}))
-                  ["#ff6600" "#666666" "#ffc0cb" "#0000ff" "#999900"])]))
-
-(defn rhombohedron [s alpha beta gamma]
-  (let [[skew-alpha skew-beta skew-gamma] (map #(- % (/ js/Math.PI 2)) [alpha beta gamma])]
-    [:div
-     [transform ;; front
-      [(m/translate-z (* s (js/Math.sin gamma) (js/Math.sin beta) 0.5))
-       (m/translate-y (* s (js/Math.cos beta) (js/Math.sin gamma) 0.5))
-       (m/translate-x (- (* s (js/Math.cos gamma) 0.5)))
-       (m/skew-x skew-alpha)
-       (m/scale-y (js/Math.cos skew-alpha))]
-      (component/rect {:w s :h s} {:background-color "#9966ff" :opacity 0.5})]
-     [transform ;; back
-      [(m/translate-z (- (* s (js/Math.sin gamma) (js/Math.sin beta) 0.5)))
-       (m/translate-y (- (* s (js/Math.cos beta) (js/Math.sin gamma) 0.5)))
-       (m/translate-x (* s (js/Math.cos gamma) 0.5))
-       (m/rotate-y js/Math.PI)
-       (m/rotate-x js/Math.PI)
-       (m/skew-x skew-alpha)
-       (m/scale-y (js/Math.cos skew-alpha))]
-      (component/rect {:w s :h s} {:background-color "#9966ff" :opacity 0.5})]
-     [transform ;; right
-      [; (m/translate-x (/ s 2))
-       ; (m/translate-z (* s (js/Math.cos gamma) 0.5))
-       ; (m/translate-y (* s (js/Math.sin gamma) (js/Math.sin alpha) 0.5))
-       ; (m/translate-x (* s (js/Math.sin gamma) (js/Math.cos alpha) 0.5))
-       ; (m/translate-x (* s (js/Math.sin gamma) (js/Math.cos alpha) 0.5))
-       ; (m/translate-x (* s (js/Math.sin gamma) (js/Math.sin alpha) 0.5))
-       ; (m/translate-y (- (* s (js/Math.cos alpha) 0.5)))
-       (m/rotate-y gamma)
-       (m/rotate-z skew-beta)
-       (m/rotate-x (- alpha (/ js/Math.PI 2)))
-       (m/skew-x skew-beta)
-       (m/scale-y (js/Math.cos skew-beta))]
-      (component/rect {:w s :h s} {:background-color "#0066ff" :opacity 0.5})]
-     ; [transform ;; left
-     ;  [(m/translate-x (- (/ s 2)))
-     ;   (m/rotate-y (+ js/Math.PI gamma))
-     ;   (m/rotate-z (- skew-beta))
-     ;   (m/rotate-x (- (- alpha (/ js/Math.PI 2))))
-     ;   (m/rotate-x js/Math.PI)
-     ;   (m/skew-x skew-beta)
-     ;   (m/scale-y (js/Math.cos skew-beta))]
-     ;  (component/rect {:w s :h s} {:background-color "#0066ff" :opacity 0.5})]
-     [transform ;; top
-      [(m/translate-y (- (* s (js/Math.sin (- js/Math.PI alpha)) 0.5))) ;; accounts for height of front face
-       (m/translate-x (- (* s (js/Math.cos (- js/Math.PI alpha)) 0.5))) ;; accounts for skew of front fact
-       (m/rotate-x beta)
-       (m/skew-x skew-gamma)
-       (m/scale-y (js/Math.cos skew-gamma))]
-      (component/rect {:w s :h s} {:background-color "#ff6699" :opacity 0.5})]
-     [transform ;; bottom
-      [(m/translate-y (* s (js/Math.sin (- js/Math.PI alpha)) 0.5))
-       (m/translate-x (* s (js/Math.cos (- js/Math.PI alpha)) 0.5))
-       (m/rotate-x (+ js/Math.PI beta))
-       (m/rotate-x js/Math.PI)
-       (m/skew-x skew-gamma)
-       (m/scale-y (js/Math.cos skew-gamma))]
-      (component/rect {:w s :h s} {:background-color "#ff6699" :opacity 0.5})]]))
+(defn on-mount [component]
+  (let [canvas (reagent/dom-node component)
+        gl (.getContext canvas "webgl")]
+    (doto gl
+      (.clearColor 0.0 0.0 0.0 1.0)
+      (.enable (.-DEPTH_TEST gl))
+      (.depthFunc (.-LEQUAL gl))
+      (.clear (bit-or (.-COLOR_BUFFER_BIT gl) (.-DEPTH_BUFFER_BIT gl))))))
 
 (defn scene []
-  (let [rotate (atom {:x (- (/ js/Math.PI 6)) :y 0})
-        start-pos (atom nil)
-        record-pos (fn [e] (reset! start-pos [[(.-pageX e) (.-pageY e)] @rotate]))
-        update-rotate (fn [e] (if @start-pos
-                                (let [current-pos   [(.-pageX e) (.-pageY e)]
-                                      [dx dy]       (map - (first @start-pos) current-pos)
-                                      {:keys [x y]} (second @start-pos)]
-                                  (reset! rotate {:y (+ y (* dx 0.001)) :x (+ x (* dy 0.001))}))))
-        clear-pos #(reset! start-pos nil)]
-    (fn []
-      [:div {:id "scene"
-             :on-mouse-down record-pos
-             :on-mouse-move update-rotate
-             :on-mouse-up clear-pos}
-       [transform
-        [(m/rotate-x (:x @rotate))
-         (m/rotate-y (:y @rotate))]
-        ;; [dodecahedron 7.5]
-        [transform
-         [(m/translate-x 0)]
-         [transform
-          [
-          (m/skew-x (/ js/Math.PI 5))
-          (m/scale-x (js/Math.cos (/ js/Math.PI 5)))
-          (m/rotate-y (/ js/Math.PI 2))
-          (m/skew-x (/ js/Math.PI 4))
-          (m/scale-x (js/Math.cos (/ js/Math.PI 4)))
-          ]
-          [rhombohedron 9 (* js/Math.PI 0.5) (* js.Math.PI 0.5) (* js.Math.PI 0.5)]]] ;; cube
-        ; [transform
-        ;  [(m/translate-x -14)]
-        ;  [rhombohedron 9 (* js/Math.PI 0.75) (* js.Math.PI 0.5) (* js.Math.PI 0.5)]]
-        ; [transform
-        ;  [(m/translate-x 0)]
-        ;  [rhombohedron 9 (/ js/Math.PI 3) (/ js.Math.PI 2) (/ js.Math.PI 4)]]
-        ; [transform
-        ;  [(m/translate-x 14)]
-        ;  [rhombohedron 9 (* js/Math.PI 0.5) (/ js.Math.PI 4) (* js.Math.PI 0.5)]]
-        ; [transform
-        ;  [(m/translate-x 28)]
-        ;  [rhombohedron 9 (* js/Math.PI 0.5) (* js.Math.PI 0.5) (/ js.Math.PI 6)]]
-        ]])))
+  [:canvas {:width 640
+            :height 480}])
 
-(reagent/render-component [scene]
+(reagent/render-component [(with-meta scene {:component-did-mount on-mount})]
                           (. js/document (getElementById "app")))
 
 
